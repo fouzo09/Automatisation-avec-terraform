4f8ffad38f94057f555f28406a343158f5f9894f
Mafouz DIALLO <mafouzdiallo@gmail.com>
2023-04-18 15:26:42 +0000

    Add lab 6

---
+++ lab6/ec2.tf
@@ -0,0 +1,39 @@
+
+resource "aws_key_pair" "MyEc2KeyPair" {
+    key_name = "MyEc2KeyPair"
+    public_key = file("./terraform.pub")
+}
+
+resource "aws_security_group" "WebServerSG" {
+  name        = "WebServerSG"
+  description = "Manage traffic"
+  
+  ingress {
+    description      = "Allow SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "WebServerSG"
+  }
+}
+
+resource "aws_instance" "MyWebServer" {
+    ami = "ami-007855ac798b5175e"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.MyEc2KeyPair.key_name
+    vpc_security_group_ids = [aws_security_group.WebServerSG.id]
+
+    tags = {
+        "Name" = "MyWebServer"
+    }
+}
---
+++ lab6/main.tf
@@ -0,0 +1,5 @@
+//CREATE EC2, SECURITY GROUP AND KEY PAIR.
+
+provider "aws" {
+    region = "us-east-1"
+}
